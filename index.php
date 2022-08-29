05871c999504ce94dcfb2f7883d34f88bb44f514
parham <Parham_minoo@yahoo.com>
2022-08-29 06:08:03 -0700

    adding first files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 1);
+
+// Require composer autoloader
+
+//define("DIR", __FILE__);
+
+define("DIR", "");
+
+date_default_timezone_set("Asia/Tehran");
+
+
+
+
